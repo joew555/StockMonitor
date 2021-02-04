41f1b70fa408a9d7cddb8c5bbca231ffed1f014d
Joe Weiss <joe.weiss@hs2solutions.com>
2021-02-04 10:40:45 -0600

    updated remove spaces method and files

--- venv/Music.py
+++ venv/Music.py
@@ -4,18 +4,18 @@ from playsound import playsound
 
 
 def removeSpaces(string):
-    return string.replace(" ","\ ")
+    return string.replace(" ","%20")
 
-downSongs = ['music/down/Crazy_Train.mp3', 'music/down/Master_of_Puppets.mp3', 'music/down/Metallica_One.mp3']
-upSongs = ['music/up/Move_Your_Feet.mp3']
+downSongs = ['music/down/Crazy Train.mp3', 'music/down/Master_of_Puppets.mp3', 'music/down/Metallica_One.mp3']
+upSongs = ['music/up/Move Your Feet.mp3']
 
 def up():
     for upSong in upSongs:
-        playsound(upSong)
+        playsound(removeSpaces(upSong))
 
 
 
 def down():
    for downSong in downSongs:
-       playsound(downSong)
+       playsound(removeSpaces(downSong))
 
